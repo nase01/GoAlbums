ca4db7994a4de073474ceb2b63d45f68cde8a0d5
Sony <nasesony01@gmail.com>
2024-06-15 22:56:46 +0800

    Move db config to .env

--- config/config.go
+++ config/config.go
@@ -1,10 +1,10 @@
 package config
 
 import (
-	"embed"
 	"encoding/json"
 	"log"
 	"os"
+	"strconv"
 
 	"GoAlbums/internal/db"
 
@@ -12,18 +12,35 @@ import (
 )
 
 var (
-	//go:embed db.json
-	res     embed.FS
 	UseDB   = true
 	DBCreds = db.DBCredentials{}
 	jwtKey  []byte
 )
 
 func LoadCreds() {
-	data, _ := res.ReadFile("db.json")
-	err := json.Unmarshal(data, &DBCreds)
+	IsPrivatePath, err := strconv.ParseBool(viper.GetString("DB_IS_PRIVATE_PATH"))
 	if err != nil {
-		log.Printf("Credentials Error: %v", err)
+		log.Printf("Error parsing DB_IS_PRIVATE: %v", err)
+		os.Exit(1)
+	}
+
+	dbConfig := map[string]interface{}{
+		"HostPath":      viper.GetString("DB_HOST_PATH"),
+		"Username":      viper.GetString("DB_USERNAME"),
+		"Password":      viper.GetString("DB_PASSWORD"),
+		"Database":      viper.GetString("DB_NAME"),
+		"IsPrivatePath": IsPrivatePath,
+	}
+
+	data, err := json.Marshal(dbConfig)
+	if err != nil {
+		log.Printf("Error marshaling dbConfig to JSON: %v", err)
+		os.Exit(1)
+	}
+
+	err = json.Unmarshal(data, &DBCreds)
+	if err != nil {
+		log.Printf("Error unmarshaling JSON to DBCreds: %v", err)
 		os.Exit(1)
 	}
 }
--- utils/validator/forms/user_validator.go
+++ utils/validator/forms/user_validator.go
@@ -21,7 +21,7 @@ func ValidateUser(user dto.SignUpRequest) error {
 	}
 
 	if !validator.StrongPassword(user.Password) {
-		return errors.New("password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
+		return errors.New("password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
 	}
 
 	return nil
