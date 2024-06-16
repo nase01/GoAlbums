d90dbeff5320b9a9cba5b4d78b4134839795e71f
Sony <nasesony01@gmail.com>
2024-06-16 20:36:28 +0800

    Add role

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,8 +1,28 @@
 package models
 
+import (
+	"gorm.io/gorm"
+)
+
+type Role string
+
+const (
+	RoleUser  Role = "user"
+	RoleAdmin Role = "admin"
+	RoleSuper Role = "super"
+)
+
 type User struct {
 	CtmBasemModel
 	Email    string `json:"email" gorm:"column:email;unique;index"`
 	Password string `json:"-" gorm:"column:password;varchar(255);"` // Exclude Password from JSON Output
 	FullName string `json:"fullname" gorm:"column:fullname;not null;varchar(255);"`
+	Role     Role   `json:"role" gorm:"column:role;type:enum('user', 'admin', 'super');default:'user'"`
+}
+
+func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
+	if user.Role == "" {
+		user.Role = RoleUser
+	}
+	return
 }
