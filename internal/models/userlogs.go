7b4a6e6a6c8668c9b2d3afaf75a295cb5065df54
Sony <nasesony01@gmail.com>
2024-06-22 16:14:59 +0800

    Remove unique

--- internal/models/userlogs.go
+++ internal/models/userlogs.go
@@ -2,6 +2,6 @@ package models
 
 type UserLogs struct {
 	CtmBasemModel
-	UserId   string `json:"userId" gorm:"column:userId;unique;not null;varchar(50);"`
+	UserId   string `json:"userId" gorm:"column:userId;not null;varchar(50);"`
 	Activity string `json:"activity" gorm:"column:activity;type:text;"`
 }
